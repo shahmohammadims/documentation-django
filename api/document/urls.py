0434dd4e298b82f8c6523e4d3ebd17cbf4d13b53
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 14:41:17 +0430

    add other urls

--- api/document/urls.py
+++ api/document/urls.py
@@ -5,5 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('language', views.LanguageView)
+router.register('document', views.Document)
+router.register('page', views.Page)
 
 urlpatterns = router.urls
