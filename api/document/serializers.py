dccc4b9b95e4b369a5bb758ef149a78c3675f363
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 15:25:51 +0430

    rename classes

--- api/document/serializers.py
+++ api/document/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from .models import Language, Document, Page
 
 
-class Languageserializer(serializers.ModelSerializer):
+class LanguageSerializer(serializers.ModelSerializer):  
     class Meta:
         model = Language
         fields = '__all__'
 
 
-class Documentserializer(serializers.ModelSerializer):
+class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = '__all__'
 
 
-class Pageserializer(serializers.ModelSerializer):
+class PageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = '__all__'
--- api/document/views.py
+++ api/document/views.py
@@ -1,18 +1,18 @@
 from .models import Language, Document, Page
-from .serializers import Languageserializer, Documentserializer, Pageserializer
+from .serializers import LanguageSerializer, DocumentSerializer, PageSerializer
 from rest_framework import viewsets
 
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
-    serializer_class = Languageserializer
+    serializer_class = LanguageSerializer
 
 
 class DocumentView(viewsets.ModelViewSet):
     queryset = Document.objects.all()
-    serializer_class = Documentserializer
+    serializer_class = DocumentSerializer
 
 
 class PageView(viewsets.ModelViewSet):
     queryset = Page.objects.all()
-    serializer_class = Pageserializer
+    serializer_class = PageSerializer
