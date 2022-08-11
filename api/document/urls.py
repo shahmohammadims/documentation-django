a777413f673f8495ab93586c094bc96372689a59
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 15:02:49 +0430

    update url

--- api/document/urls.py
+++ api/document/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
 from rest_framework import routers
-
 
 router = routers.DefaultRouter()
 router.register('language', views.LanguageView)
