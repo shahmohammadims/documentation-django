2903811e27df814eadb9ff1993a07b40bf05007b
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-12 01:28:44 +0430

    rename server to localhost

--- api/config/settings.py
+++ api/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
