3d0847bfee3ae31e4212f57d94eda1f6e5c68829
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 14:38:02 +0430

    change wrong type

--- api/users/models.py
+++ api/users/models.py
@@ -47,7 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
-        return self.user_name
+        return self.username
     
     class Meta:
         ordering = ['-id']
