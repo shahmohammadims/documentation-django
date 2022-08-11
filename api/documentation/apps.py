e9bc6c02c7723c5c933b9546089110d52c9cea8b
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 15:43:33 +0430

    fixen bug name in documanetation/app

--- api/documentation/apps.py
+++ api/documentation/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DocumentationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'document'
+    name = 'documentation'
