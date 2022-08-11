792c11becc301ca68a277ff541da71ef9520e567
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-11 07:32:06 +0430

    create models documentation

--- api/documentation/models.py
+++ api/documentation/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(verbose_name='نام' ,max_length=50)
+    slug = models.SlugField(verbose_name='لینک' ,max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'زبان'
+        verbose_name_plural = 'زبان ها'
+
+
+class Documentation(models.Model):
+    language = models.ForeignKey(Language, models.SET_NULL, verbose_name='زبان', null=True)
+    name = models.CharField(verbose_name='نام' ,max_length=50)
+    slug = models.SlugField(verbose_name='لینک' ,max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'داکیومنت'
+        verbose_name_plural = 'داکیومنت ها'
+
+
+class Page(models.Model):
+    documentation = models.ForeignKey(Documentation, models.SET_NULL, verbose_name='داکیومنت', null=True)
+    title = models.CharField(verbose_name='عنوان' ,max_length=100,)
+    slug = models.SlugField(verbose_name='لینک' ,max_length=50)
+    body = models.TextField(verbose_name='متن')
+    date_create = models.DateField(verbose_name='زمان انتشار', auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'صفحه'
+        verbose_name_plural = 'صفحه ها'
+    
