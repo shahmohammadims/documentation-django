bb6e8d594183cb8be2f5fed6ddc715be5ab934b0
Mahdi Shahmohammadi <shahmohammadiemes@gmail.com>
2022-08-17 13:50:24 +0430

    fixed bug url static

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('user/', include('rest_framework.urls')),
     path('documentation/', include('documentation.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
